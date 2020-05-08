3ffb5b327c68d243b4d3ad85ce8c15560616c39a
Omid Nejati <omid@tonal.com>
2020-05-08 15:15:12 -0700

    hello world!

--- hello_service/main.go
+++ hello_service/main.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"fmt"
-	"html"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Hellov1, %q", html.EscapeString(r.URL.Path))
+	fmt.Printf("Hello World!")
 }
