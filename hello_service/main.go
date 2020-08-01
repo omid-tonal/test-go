a60aa6f659e618c7731bedcc363cd83a5eb6134b
Omid Nejati <omid@tonal.com>
2020-07-31 20:14:54 -0700

    change version to 1.1.1

--- hello_service/main.go
+++ hello_service/main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Tonal v1.1.0 %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello Tonal v1.1.1 %s!", r.URL.Path[1:])
 }
--- test_service/main.go
+++ test_service/main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func TestServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test Tonal v1.1.0 %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Test Tonal v1.1.1 %s!", r.URL.Path[1:])
 }
